bfe7472601dd086f76087021e684761ee9518f6e
sohagroy7223 <freelancersohagroy@gmail.com>
2026-03-29 10:20:08 +0600

    addad dragon news first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-center mt-10 font-bold text-green-600">
+        Get started
+      </h1>
+    </>
+  );
+}
+
+export default App;
