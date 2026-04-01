04275d330795934fcf5b748a6db66078ea55dba7
sohagroy7223 <freelancersohagroy@gmail.com>
2026-04-02 01:08:09 +0600

    addad on Authstate change function if user sign upc, login, and log out every render it notice

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { use } from "react";
 import { Link, NavLink } from "react-router";
-import user from "../assets/user.png";
+import userI from "../assets/user.png";
+import { AuthContext } from "../Context/AuthContext";
 
 const Navbar = () => {
+  const { user } = use(AuthContext);
+
   return (
     <div className="flex justify-between">
-      <div className=""></div>
+      <div className="">{user && user.email}</div>
       <div className="nav flex gap-4 items-center">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/About">About</NavLink>
         <NavLink to="/career">Career</NavLink>
       </div>
       <div className="login flex gap-3">
-        <img src={user} alt="user" />
-        <Link to="/login">
-          <button className="btn btn-primary">Login</button>
-        </Link>
+        <img src={userI} alt="user" />
+        {user ? (
+          <button className="btn btn-primary">Log out</button>
+        ) : (
+          <Link to="/login">
+            <button className="btn btn-primary">Login</button>
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../Firebase/firebase.config";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
   const SignUpUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
@@ -15,7 +18,22 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        console.log("has User", currentUser);
+        setUser(currentUser);
+      } else {
+        console.log("don't have", currentUser);
+      }
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
   const userInfo = {
+    user,
     SignUpUser,
     SignInUser,
   };
